9b2ced3e063e1265dd4dfda31d885ce2f32fa3fb
Ashley Whitehouse <ash4669@hotmail.co.uk>
2022-03-09 16:51:43 +0000

    cleaned string creation

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,10 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location. Please try again', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out with humidity of ' + body.current.humidity + '%.' )
+            callback(
+                undefined,
+                body.current.weather_descriptions + '. It is currently ' + body.current.temperature + ' degrees out.' 
+                + ' It feels like ' + body.current.feelslike + ' degrees out with humidity of ' + body.current.humidity + '%.' )
         }
     })
 }
